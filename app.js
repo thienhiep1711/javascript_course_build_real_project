680758ef19fa99f9e18d1a5f478191348eebd52e
Hiep Nguyen <thienhiep1711@users.noreply.github.com>
2019-03-25 15:41:06 +0700

    add nodejs servers

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+
+
+const port = process.env.PORT || 3000;
+var app = express();
+
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url} `
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server log.');
+    }
+  });
+  next();
+})
+
+app.use(express.static(__dirname + '/dist'));
+app.get('/', (req, res) => {
+  res.render('index.html');
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
