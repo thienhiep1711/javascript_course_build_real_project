d32e52587f5a97d761e6fd8c530556b40290120d
Hiep Nguyen <thienhiep1711@users.noreply.github.com>
2019-03-25 16:09:56 +0700

    fix deployment

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const hbs = require('hbs');
 const fs = require('fs');
-
-
 
 const port = process.env.PORT || 3000;
 var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
 
 
 app.use((req, res, next) => {
@@ -19,7 +21,44 @@ app.use((req, res, next) => {
   next();
 })
 
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// })
+
 app.use(express.static(__dirname + '/dist'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+})
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+} )
+
+app.get('/', (req, res) => {
+  // res.send('<h1>Hello Express</h1>')
+  res.render('home.hbs', {
+    pageTitle: 'Home page',
+    welcomeMessage: 'Welcome to my website'
+  })
+
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request',
+  })
+});
+
+
+
 
 
 app.listen(port, () => {
